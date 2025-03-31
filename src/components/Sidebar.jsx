cc4767006ca3455481bce39b3c4903fc75a58088
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-31 14:09:39 -0700

    test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -60,7 +60,7 @@ console.log(user)
             {/* Main Links */}
             <ul className="sidebar_list">
               <li><NavLink to="/dashboard" onClick={toggleSidebar} className={({ isActive }) => isActive ? "active-class" : "inactive-class"}>Dashboard</NavLink></li>
-              {/* <li><NavLink to="/schedule" onClick={toggleSidebar} className={({ isActive }) => isActive ? "active-class" : "inactive-class"}>Schedule</NavLink></li> */}
+              <li><NavLink to="/dashboard/gameToggle" onClick={toggleSidebar} className={({ isActive }) => isActive ? "active-class" : "inactive-class"}>Schedule</NavLink></li>
               <li><NavLink to="/scheduleBuilder" onClick={toggleSidebar} className={({ isActive }) => isActive ? "active-class" : "inactive-class"}>Builder</NavLink></li>
             </ul>
 
