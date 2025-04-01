47549a9584868076ba1b9403e171cbb64d06f666
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-31 17:58:31 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -53,12 +53,13 @@ import ScheduleBuilder from './pages/ScheduleBuilder/ScheduleBuilder';
 
 
 function App() {
+  const location = useLocation();
   const [searchParams] = useSearchParams();
   const pathDomain = location.pathname.split('/')[1]; // grabs 'some-domain' from '/some-domain'
   const domain = searchParams.get("domain") || window.location.hostname;
   const [userForDomain, setUserForDomain] = useState(null);
   const [loadingDomain, setLoadingDomain] = useState(true);
-  const location = useLocation();
+
   const { loading, isAuthenticated } = useAuth();
 
   const isPublicView = (domain || location.pathname === "/") && !isAuthenticated;
